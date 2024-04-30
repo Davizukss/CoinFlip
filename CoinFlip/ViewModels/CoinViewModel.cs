8f4965efcc762e9cb4210488b11974dfd47aafdc
davizuks <davivarelladev@gmail.com>
2024-04-30 12:35:31 -0300

    binding

---
+++ CoinFlip/ViewModels/CoinViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoinFlip.Models;
+using System.Windows.Input;
+
+namespace CoinFlip.ViewModels
+{
+    internal partial class CoinViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private string _lado;
+
+        [ObservableProperty]
+        private string _ladoEscolhido;
+
+        [ObservableProperty]
+        private string _resultado;
+
+        public CoinViewModel() {
+            JogarCommand = new Command(Jogar);
+        }
+
+        public ICommand JogarCommand { get; }
+
+        public void Jogar()
+        {
+            Coin coin = new Coin();
+            Resultado = coin.Jogar(LadoEscolhido);
+        }
+    }
+}
